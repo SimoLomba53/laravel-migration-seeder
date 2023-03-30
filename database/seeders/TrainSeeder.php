866c589ebc430d019b693f442c78e061f8d9a0ce
SimoLomba53 <simolomba253@gmail.com>
2023-03-30 17:30:51 +0200

    problema risolto

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -20,12 +20,12 @@ class TrainSeeder extends Seeder
 
         $new_train->Azienda = '';
         $new_train->Stazione_di_partenza = '';
-        $new_train->Stazione_di_arrivo = '';
+        $new_train->Stazione_di_arrivo = 'aaa';
         $new_train->Orario_di_partenza = '';
         $new_train->Orario_di_arrivo = '';
         $new_train->Codice_treno = '';
-        $new_train->Numero_carrozze = '';
-        $new_train->In_orario = 'f';
+        $new_train->Numero_carrozze = '5';
+        $new_train->In_orario = '';
         $new_train->Cancellato = '';
 
         $new_train->save();
